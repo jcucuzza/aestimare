d8f2d2f8c028195e008bb56689d2f2d5e47f13f8
jcucuzza <smackthatcow@gmail.com>
2022-10-22 01:55:58 -0400

    no more ranker

--- ae-landfill/src/test/java/com/ae/landfill/RankerApplicationTests.java
+++ ae-landfill/src/test/java/com/ae/landfill/RankerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RankerApplicationTests {
+class LandfillApplicationTests {
 
 	@Test
 	void contextLoads() {
