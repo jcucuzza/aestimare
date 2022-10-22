47847f695ba4bc5d2aa59f7e53a89c22ecd488e0
jcucuzza <smackthatcow@gmail.com>
2022-10-22 14:04:57 -0400

    added initialization process on drawings test

--- ae-landfill/src/test/java/com/ae/landfill/model/DrawingsTest.java
+++ ae-landfill/src/test/java/com/ae/landfill/model/DrawingsTest.java
@@ -1,5 +1,6 @@
 package com.ae.landfill.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -10,13 +11,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DrawingsTest {
 
+    private Drawing drawing;
+    @BeforeEach
+    void setUp() {
+        drawing = new Drawing();
+    }
+
     @ParameterizedTest
     @CsvSource({
             "2022-01-01T00:00:00, 2022-01-01",
             "2022-01-02, 2022-01-02"
     })
     void setDrawDateTest(String actual, String expected) {
-        Drawing drawing = new Drawing();
         drawing.setDrawDate(actual);
         actual = drawing.getDrawDate();
         assertEquals(expected, actual, actual + " date length and format should appear as expected ->  " + expected);
@@ -27,7 +33,6 @@ public class DrawingsTest {
             "2022-01-01, 1640995200000"
     })
     void getEpochTime(String date, long expected ) throws ParseException {
-        Drawing drawing = new Drawing();
         drawing.setDrawDate(date);
         long actual = drawing.getEpochTime();
         assertEquals(expected, actual, actual + " date conversion into milliseconds should appear as expected ->  " + expected);
